f64fe15eb6074694ee962bc6efeb5a152f17f78a
kubade220 <32162454+kubade220@users.noreply.github.com>
2023-10-25 20:14:44 +0200

    Update Footer.tsx

--- app/(site)/components/global/Footer.tsx
+++ app/(site)/components/global/Footer.tsx
@@ -8,11 +8,11 @@ export default function Footer() {
 
         <small className="hover:text-white duration-200">
           <a
-            href="https://github.com/Evavic44/sanity-nextjs-site"
+            href="https://github.com/kubaade220/sanity-nextjs-site"
             target="_blank"
             rel="noreferrer noopener"
           >
-            Prepared by <span className="text-green-400">Victor Eke</span>
+            Prepared by <span className="text-green-400">Jakub Nabywaniec</span>
           </a>
         </small>
       </div>
