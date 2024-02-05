d914bee99ddcd37ed3e1ebd77ec8d72efd3e9529
kubade220 <32162454+kubade220@users.noreply.github.com>
2024-02-05 19:48:52 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@ import { schemaTypes } from "./schemas";
 
 export default defineConfig({
   name: "sanity-nextjs-site",
-  title: "Sanity Next.js Site",
+  title: "Jakub Nabywaniec",
   projectId: "uc8l01he",
   dataset: "production",
   basePath: "/studio",
