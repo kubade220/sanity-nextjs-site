2d2fb2a532d52fed4fba39d01d82cc72492cc964
kubade220 <32162454+kubade220@users.noreply.github.com>
2024-04-06 20:00:42 +0200

    Update Footer.tsx

--- app/(site)/components/global/Footer.tsx
+++ app/(site)/components/global/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
             target="_blank"
             rel="noreferrer noopener"
           >
-            Prepared by <span className="text-green-400">Jakub Nabywaniec</span>
+            Designed by <span className="text-green-400">Jakub Nabywaniec</span>
           </a>
         </small>
       </div>
